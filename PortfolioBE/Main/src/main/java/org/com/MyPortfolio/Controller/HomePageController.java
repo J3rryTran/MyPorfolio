4aa7d94ef5dd8e9eabbcb598082a83282e911490
j3rrytr <trungtran20802@gmail.com>
2025-03-18 09:09:26 +0700

    Upload BE

--- PortfolioBE/Main/src/main/java/org/com/MyPortfolio/Controller/HomePageController.java
+++ PortfolioBE/Main/src/main/java/org/com/MyPortfolio/Controller/HomePageController.java
@@ -1,13 +1,17 @@
 package org.com.MyPortfolio.Controller;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomePageController {
+    @Autowired
+    private HomePageController
+
     @RequestMapping(value = {"", "/", "home"})
-    public String displayHomePage(){
-        return;
+    public String HomePage(){
+
     }
 }
