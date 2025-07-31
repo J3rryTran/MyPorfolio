c9bac7c6c889a4d223c83c70257ee666282acf27
J3rryTran <trungtran20802@gmail.com>
2025-07-31 15:58:14 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Header from './components/Header';
-import About from './components/About';
-import Experience from './components/Experience';
-import Education from './components/Education';
+import Header from './components/Header/Header';
+import About from './components/About/About';
+import Experience from './components/Experience/Experience';
+import Education from './components/Education/Education';
 import './App.css';
 
 function App() {
